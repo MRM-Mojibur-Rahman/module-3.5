188530e123c00367e010a0acf0106dcef349d283
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2025-06-25 18:51:04 +0600

    problem 2 solved

--- problem2.cpp
+++ problem2.cpp
@@ -1,38 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main ()
+int main()
 {
-   int n ;
-   cin>>n;
-   vector<int> v(n);
-   vector<int> vsum(n);
-   for (int i = 0; i < n; i++)
-   {
-    cin>>v[i];
-    if (i==0)
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    vector<int> vsum(n);
+    for (int i = 0; i < n; i++)
     {
-        vsum[0]=v[0];
-        
+        cin >> v[i];
+        if (i == 0)
+        {
+            vsum[0] = v[0];
+        }
+        else
+        {
+            vsum[i] = v[i] + vsum[i - 1];
+        }
     }
-    else
+    for (int i = 0; i < n; i++)
     {
-        vsum[i]=v[i]+vsum[i-1];
+        int low = vsum[i - 1];
+        int high = vsum[n - 1] - vsum[i];
+        if (low == high)
+        {
+            cout << i;
+            break;
+        }
     }
 
-   }
-   for (int i = 0; i < n; i++)
-   {
-       int low = vsum[i-1];
-       int high = vsum[n-1]-vsum[i];
-       if (low==high)
-       {
-       cout<<i;
-       }
-       
-
-   }
-   
-
-   
     return 0;
 }
